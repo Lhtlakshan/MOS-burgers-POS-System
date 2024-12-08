a189fe9c93e909a054ce47fbfc080eb9a24a35f3
Thilina <thilinalakshanhewage1999@gmail.com>
2024-12-08 08:54:16 +0530

    updated

--- js/login.js
+++ js/login.js
@@ -1,9 +1,11 @@
 let email = document.getElementById("email");
 let password = document.getElementById("password");
 
-if (email === "admin@gmail.com" && password === "admin123") {
-  alert("Welcome, Admin!");
-  window.location.href = "../admin.html";
-} else {
-  window.location.href = "../customer_work.html";
+let log = ()=>{
+    if (email === "admin@gmail.com" && password === "admin123") {
+      alert("Welcome, Admin!");
+      window.location.href = "../admin.html";
+    } else {
+      window.location.href = "../customer_work.html";
+    }
 }
